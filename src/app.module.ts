f7394c352ea0d9b299e3b4a0fb2dc03e82f18aa1
ranarasul221 <miahrana485@gmail.com>
2026-04-14 05:59:32 +0600

    connect all module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,15 @@ import { AuthModule } from 'module/auth/auth.module';
 import { UsersModule } from 'module/users/users.module';
 import { MailModule } from 'common/mail/mail.module';
 import { ContentMasterModule } from 'module/content-master/content-master.module';
+import { ContactInquiryModule } from 'module/contact-inquiry/contact-inquiry.module';
+import { ContentModule } from 'module/content/content.module';
+import { ContentAccessModule } from 'module/content-access/content-access.module';
+import { ContentAssetModule } from 'module/content-asset/content-asset.module';
+import { PageModule } from 'module/page/page.module';
+import { ServiceModule } from 'module/service/service.module';
+import { BillingModule } from 'module/billing/billing.module';
+import { WebhookModule } from 'module/webhook/webhook.module';
+import { PlanModule } from 'module/plan/plan.module';
 
 
 @Module({
@@ -21,9 +30,16 @@ import { ContentMasterModule } from 'module/content-master/content-master.module
     MailModule,
     AuthModule,
     UsersModule,
-    ContentMasterModule
-    
-
+    ContentModule,
+    ContentAccessModule,
+    ContentMasterModule,
+    ContactInquiryModule,
+    ContentAssetModule,
+    PageModule,
+    ServiceModule,
+    PlanModule,
+    BillingModule,
+    WebhookModule
   ],
 })
 export class AppModule {}
