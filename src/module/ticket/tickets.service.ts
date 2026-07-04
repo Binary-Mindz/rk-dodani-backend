e538db598c5e9e4c9f20cd361647dd3b80a6d5db
ranamiah221 <ranarasul21@gmail.com>
2026-07-04 19:09:00 +0600

    added the ticket

--- src/module/ticket/tickets.service.ts
+++ src/module/ticket/tickets.service.ts
@@ -14,6 +14,7 @@ export class TicketsService {
             const ticket = await tx.contactInquiry.create({
                 data: {
                     name: dto.name,
+                    ticketId: dto.ticketId,
                     email: dto.email,
                     subject: dto.subject,
                     message: dto.message,
