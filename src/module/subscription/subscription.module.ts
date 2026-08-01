2e2dc0ea13e756394dfabf9efb8c4da570b89c28
ridoybabu781 <itzking.rk@gmail.com>
2026-08-01 17:18:15 +0600

    add MailModule to subscription module imports

--- src/module/subscription/subscription.module.ts
+++ src/module/subscription/subscription.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { SubscriptionController } from './subscription.controller';
 import { SubscriptionService } from './subscription.service';
 import { AuditModule } from '../audit/audit.module';
+import { MailModule } from 'common/mail/mail.module';
 
 @Module({
-  imports: [ConfigModule, AuditModule],
+  imports: [ConfigModule, AuditModule, MailModule],
   controllers: [SubscriptionController],
   providers: [SubscriptionService],
   exports: [SubscriptionService],
